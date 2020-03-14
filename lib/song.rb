e81c7aef58451e4ae1a355eee4d2ccba3b14cc60
Will Corcoran <corcoranwill@gmail.com>
2020-03-14 01:28:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,18 @@
-# Song belongs to an Artist
 class Song
   attr_accessor :name, :artist
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def artist_name
-    if @artist == nil
-      nil
-    else
-      self.artist.name
-    end
+    artist.name if artist
   end
 end
