f1582239b3ae6bda59d94325c338da0c736e63f9
Will Corcoran <corcoranwill@gmail.com>
2020-03-14 01:19:16 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,1 +1,16 @@
 #Post belongs to an Author
+class Post
+  attr_accessor :name, :author
+
+  def initialize(name)
+    @name = name
+  end
+
+  def artist_name
+    if @artist == nil
+      nil
+    else
+      self.artist.name
+    end
+  end
+end
