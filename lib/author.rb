52fc543855c25e2e0266e143def94339b51aa512
Will Corcoran <corcoranwill@gmail.com>
2020-03-14 01:21:50 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,1 +1,28 @@
-# Author has many Posts 
+# Author has many Posts
+class Author
+  attr_accessor :name, :posts
+  @@post_count = 0
+
+  def self.post_count
+    @@post_count
+  end
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    @posts << post
+    @@post_count += @posts.count
+    post.artist = self
+  end
+
+  def add_post_by_name(name)
+    post = Post.new(name)
+    @posts << post
+    @@post_count += @posts.count
+    post.author = self
+    post
+  end
+end
