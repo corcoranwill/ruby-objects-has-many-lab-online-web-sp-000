781619afc01f770ef17ce346b666e3f05d349ad4
Will Corcoran <corcoranwill@gmail.com>
2020-03-14 01:08:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,8 @@
 # Artist has many Songs
 
 class Artist
-  attr_accessor :name
-  @@all = []
+  attr_accessor :name, :songs
+  @@song_count = 0
 
   def initialize(name)
     @name = name
@@ -18,12 +18,35 @@ class Artist
   end
 
   def song_count
-    Song.all.count
+    @@song_count
   end
 end
 
 
+class Artist
+  attr_accessor :name, :songs
 
-  # def song_count
-  #   Song.all.select {|song| song.artist == self}
-  # end
+  @@song_count = 0
+  def self.song_count
+    @@song_count
+  end
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    @@song_count += @songs.count
+    song.artist = self
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    @@song_count += @songs.count
+    song.artist = self
+    song
+  end
+end
