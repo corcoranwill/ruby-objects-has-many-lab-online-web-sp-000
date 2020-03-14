a5a5557a50dcebed10bb77694c4083f45ffbdb0f
Will Corcoran <corcoranwill@gmail.com>
2020-03-14 00:59:42 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,3 +13,14 @@ class Artist
   end
 
 end
+
+
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def add_song_by_name(name, genre)
+    song = Song.new(name, genre)
+    add_song(song)
+  end
